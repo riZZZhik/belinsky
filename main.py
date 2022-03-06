a5a732d160dda29d10ba6e0294dd12297e0fcfe0
Dmitry Barsukoff <riZZZhik@gmail.com>
2022-03-06 17:42:28 +0300

    [ADD] "add-new-word" request

--- main.py
+++ main.py
@@ -9,4 +9,9 @@ word_finder = WordFinder("123")
 app = Flask("WordFinder")
 
 # Add handlers
-app.add_url_rule('/get-all-words', view_func=word_finder.get_all_words, methods=["GET"])
+app.add_url_rule('/get-all-words', view_func=word_finder.get_all_words, methods=['GET'])
+app.add_url_rule('/add-new-word', view_func=word_finder.add_new_word, methods=['POST'])
+
+# Run app
+if __name__ == '__main__':
+    app.run(debug=True)
--- word_finder/word_finder.py
+++ word_finder/word_finder.py
@@ -1,6 +1,34 @@
+from flask import request
+
+
 class WordFinder:
-    def __init__(self, var):
+    def __init__(self):
         self.words = ['test', 'super_test']
+
+    def add_new_word(self):
+        # Check api request
+        if not request.json:
+            response = {
+                'error': "json body not found in request",
+                'status': 400
+            }
+            return response
+
+        if 'word' not in request.json:
+            response = {
+                'error': "key 'word' not found in request body",
+                'status': 400
+            }
+            return response
+
+        # Add new word to database
+        self.words.append(request.json['word'])
+
+        response = {
+            'result': 'ok',
+            'status': 200
+        }
+        return response
 
     def get_all_words(self):
         response = {
