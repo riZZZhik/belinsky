f48ca1192ad05a01f4471d5062ad534c4d8f83ab
Dmitry Barsukoff <riZZZhik@gmail.com>
2022-04-05 16:08:05 +0300

    [DEL] Known phrases routes

--- app/belinsky/routes/phrase_finder/__init__.py
+++ app/belinsky/routes/phrase_finder/__init__.py
@@ -8,15 +8,11 @@ def create_blueprint_phrase_finder():
     # Create Flask blueprint
     phrase_finder_bp = Blueprint('phrase_finder', __name__)
 
-    # Connect MongoDB
+    # Create PhraseFinder Worker
     worker = PhraseFinder()
 
     # Add request handlers
-    phrase_finder_bp.add_url_rule('/find-phrases', view_func=worker.find_phrases, methods=['POST'])
-    phrase_finder_bp.add_url_rule('/add-phrase', view_func=worker.add_phrase, methods=['POST'])
-    phrase_finder_bp.add_url_rule('/get-known-phrases', view_func=worker.get_known_phrases, methods=['GET'])
-    phrase_finder_bp.add_url_rule('/clear-known-phrases', view_func=worker.clear_known_phrases, methods=['POST'])
-
+    phrase_finder_bp.add_url_rule('/find-phrases', view_func=worker.find_phrases, methods=['GET'])
     return phrase_finder_bp
 
 
