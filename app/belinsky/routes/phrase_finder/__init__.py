d751da63ba531fa60ae180c2f2cc44cbe5e2f787
Dmitry Barsukoff <riZZZhik@gmail.com>
2022-04-12 23:18:40 +0300

    [FIX] Remove useless PhraseFinder class initialization

--- app/belinsky/routes/phrase_finder/__init__.py
+++ app/belinsky/routes/phrase_finder/__init__.py
@@ -1,8 +1,78 @@
 """Belinsky PhraseFinder blueprint."""
 from flask import Blueprint
+from flask import request
+from flask_login import login_required
+from prometheus_client import Summary
 
-from .phrase_finder import PhraseFinder
-from .phrase_finder_api import PhraseFinderAPI
+from .phrase_finder import PhraseFinder, UnknownLanguageError
+
+# Initialize prometheus metrics.
+FIND_PHRASES_LATENCY = Summary('pf_find_phrases_latency', 'Latency of "find-phrases" request')
+
+# Initialize PhraseFinder worker.
+phrase_finder = PhraseFinder()
+
+
+@FIND_PHRASES_LATENCY.time()
+@login_required
+def find_phrases():
+    """ Find known phrases in text.
+    ---
+    Body (JSON):
+        - text (str): Text to be processed.
+        - phrases (List[str]): Phrases to be found.
+        - language (str): Language.
+
+    Responses:
+        200:
+            description: Return found phrases and their indexes in text.
+            schema:
+                result: Dictionary with found phrases and their indexes.
+                status: 200
+        400:
+            description: Json body or 'text' key not found in request body.
+            schema:
+                error: Error description.
+                status: 400
+        500:
+            description: Unexpected error while processing request.
+            schema:
+                error: Error description.
+                status: 500
+    """
+
+    # Check request body
+    if not request.json:
+        response = {
+            'error': "Json body not found in request",
+            'status': 400
+        }
+        return response, 400
+
+    required_keys = ['text', 'phrases']
+    if not all(key in request.json for key in required_keys):
+        response = {
+            'error': f"Required keys not found in request body :{', '.join(required_keys)}.",
+            'status': 400
+        }
+        return response, 400
+
+    # Process text
+    try:
+        lang = request.json['language'] if 'language' in request.json else None
+        result = phrase_finder.find_phrases(request.json['text'], request.json['phrases'], lang)
+    except UnknownLanguageError as exception:
+        response = {
+            'error': str(exception),
+            'status': 400
+        }
+        return response
+
+    response = {
+        'result': result,
+        'status': 200
+    }
+    return response, 200
 
 
 def create_blueprint_phrase_finder():
@@ -10,12 +80,9 @@ def create_blueprint_phrase_finder():
     # Create Flask blueprint
     phrase_finder_bp = Blueprint('phrase_finder', __name__)
 
-    # Create PhraseFinder Worker
-    worker = PhraseFinderAPI()
-
     # Add request handlers
-    phrase_finder_bp.add_url_rule('/find-phrases', view_func=worker.find_phrases, methods=['GET'])
+    phrase_finder_bp.add_url_rule('/find-phrases', view_func=find_phrases, methods=['GET'])
     return phrase_finder_bp
 
 
-__all__ = ['create_blueprint_phrase_finder', 'PhraseFinderAPI', 'PhraseFinder']
+__all__ = ['create_blueprint_phrase_finder', 'PhraseFinder']
